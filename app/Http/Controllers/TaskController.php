a021a9e20f48cb793cc95a0a1c2b274646fee010
kodai.tsumagari <kodai.tsumagari@asia-quest.jp>
2023-05-30 15:52:03 +0900

    タスク削除API実装

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -30,4 +30,10 @@ class TaskController extends Controller
         return $task;
     }
 
+    public function destroy(Task $task)
+    {
+        $task->delete();
+
+        return $task;
+    }
 }
